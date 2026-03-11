176497ff445f58dde57ffb9068cf22bbc5a7ffd5
Shervvyn <d.sherwyn.jeffrey@gmail.com>
2026-03-11 09:02:24 +0530

    Add files via upload

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class  PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to palindrome checker");
+        System.out.println("version : 1.0");
+        System.out.println("System initialised successfully.");
+    }
+}
