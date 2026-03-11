50019fff91a93dcdb8a1ae0f9d5bf9b6df6bbfa7
Shervvyn <d.sherwyn.jeffrey@gmail.com>
2026-03-11 09:27:28 +0530

    Add files via upload

---
+++ UseCase12PalindromeCheckerApp.java
@@ -0,0 +1,70 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class UseCase12PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        // Choose strategy
+        PalindromeStrategy strategy = new StackStrategy();
+
+        boolean result = strategy.check(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
+
+
+/* Strategy Interface */
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+
+/* Stack Strategy */
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for(char c : input.toCharArray()) {
+            if(c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+/* Deque Strategy */
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for(char c : input.toCharArray()) {
+            deque.add(c);
+        }
+
+        while(deque.size() > 1) {
+
+            if(deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
